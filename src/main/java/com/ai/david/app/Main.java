d961e87567c8cb9765acd53e1326f99c958823ed
Veres Dávid <veres.david11@gmail.com>
2016-09-17 09:57:00 +0200

    Updated Main to read many problems from stdin.

--- src/main/java/com/ai/david/app/Main.java
+++ src/main/java/com/ai/david/app/Main.java
@@ -7,34 +7,62 @@ import com.ai.david.problem.impl.PancakePuzzleProblem;
 import com.ai.david.state.impl.PancakePuzzleState;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    PancakePuzzleState start = new PancakePuzzleState(Arrays.asList(3, 2, 5, 1, 6, 4, 7));
-    PancakePuzzleState goal = new PancakePuzzleState(7);
+    Scanner scanner = new Scanner(System.in);
+    int numberOfProblems = 0;
 
-    List<PancakePuzzleOperator> operators = new ArrayList<>();
-    for (int i = 2; i <= 7; i++) {
-      operators.add(new PancakePuzzleOperator(i));
+    while (scanner.hasNext()) {
+
+      numberOfProblems++;
+
+      int numberOfPancakes;
+      List<Integer> startPancakes = new ArrayList<>();
+      List<Integer> goalPancakes = new ArrayList<>();
+
+      numberOfPancakes = scanner.nextInt();
+
+      for (int i = 0; i < numberOfPancakes; i++) {
+        startPancakes.add(scanner.nextInt());
+      }
+
+      for (int i = 0; i < numberOfPancakes; i++) {
+        goalPancakes.add(scanner.nextInt());
+      }
+
+      PancakePuzzleState start = new PancakePuzzleState(startPancakes);
+      PancakePuzzleState goal = new PancakePuzzleState(goalPancakes);
+
+      List<PancakePuzzleOperator> operators = new ArrayList<>();
+      for (int i = 2; i <= numberOfPancakes; i++) {
+        operators.add(new PancakePuzzleOperator(i));
+      }
+
+      PancakePuzzleProblem problem = new PancakePuzzleProblem(start, goal, operators);
+
+      PancakePuzzleAStar
+          pancakePuzzleAStar =
+          new PancakePuzzleAStar(problem, new PancakePuzzleManhattanHeuristic());
+
+      List<PancakePuzzleOperator> solution = pancakePuzzleAStar.search();
+
+      System.out.println("PROBLEM #" + numberOfProblems + ":");
+
+      for (PancakePuzzleOperator operator : solution) {
+        System.out.print(operator.getNumberOfPancakesToReverse() + " ");
+      }
+      System.out.println();
+
+      System.out.println("-------------------------------");
+
     }
 
-    PancakePuzzleProblem problem = new PancakePuzzleProblem(start, goal, operators);
-
-    PancakePuzzleAStar
-        ppas =
-        new PancakePuzzleAStar(problem, new PancakePuzzleManhattanHeuristic());
-
-    List<PancakePuzzleOperator> result = ppas.search();
-
-    System.out.println("Megoldás:");
-    for (PancakePuzzleOperator operator : result) {
-      System.out.print(operator.getNumberOfPancakesToReverse() + " ");
-    }
-    System.out.println();
+    scanner.close();
 
   }
 
